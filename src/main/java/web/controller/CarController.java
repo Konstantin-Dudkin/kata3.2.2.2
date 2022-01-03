0b9e8753bc1d0c083325666969c23290a69c8c47
Konstantin <lord-konst@mail.ru>
2022-01-03 12:35:30 +0300

    3 commit

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,15 +3,20 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class CarController {
-    @GetMapping(value = "/car")
-    public String printWelcome(ModelMap model) {
+    @GetMapping(value = "/cars")
+    public String printWelcome(ModelMap model,
+                               @RequestParam(value = "count", required = false) Integer count) {
+
+        List<Car> table;
 
         List<Car> cars = new ArrayList<>();
         cars.add(new Car("Model_1", 1, 100));
@@ -19,7 +24,15 @@ public class CarController {
         cars.add(new Car("Model_3", 3, 200));
         cars.add(new Car("Model_4", 4, 250));
         cars.add(new Car("Model_5", 5, 300));
-        model.addAttribute("cars", cars);
+
+        if (count != null) {
+            table = cars.stream().limit(count).collect(Collectors.toList());
+        } else {
+            table = cars;
+        }
+
+
+        model.addAttribute("cars", table);
 
         return "car";
     }
