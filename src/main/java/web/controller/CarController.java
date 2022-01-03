5d63afc30d0b639e9f58d98eca19259904e62935
Konstantin <lord-konst@mail.ru>
2022-01-03 13:09:44 +0300

    4 commit

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,8 +17,6 @@ public class CarController {
     public String printWelcome(ModelMap model,
                                @RequestParam(value = "count", required = false) Integer count) {
 
-        List<Car> table;
-
         List<Car> cars = new ArrayList<>();
         cars.add(new Car("Model_1", 1, 100));
         cars.add(new Car("Model_2", 2, 150));
@@ -25,14 +24,8 @@ public class CarController {
         cars.add(new Car("Model_4", 4, 250));
         cars.add(new Car("Model_5", 5, 300));
 
-        if (count != null) {
-            table = cars.stream().limit(count).collect(Collectors.toList());
-        } else {
-            table = cars;
-        }
 
-
-        model.addAttribute("cars", table);
+        model.addAttribute("cars", CarService.getCarsTable(count, cars));
 
         return "car";
     }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,4 +1,16 @@
 package web.service;
 
-public interface CarService {
+import web.model.Car;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarService {
+    public static List<Car> getCarsTable (Integer count, List<Car> cars) {
+        if (count != null) {
+            return cars.stream().limit(count).collect(Collectors.toList());
+        } else {
+            return cars;
+        }
+    }
 }
